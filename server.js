dd51fdf75222214312e02bef4b9e8d34ae024466
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-12 13:44:36 +0300

    implement app and server

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+port = process.env.PORT;
+app.listen(port, () => {
+  console.log('Server is running...');
+});
