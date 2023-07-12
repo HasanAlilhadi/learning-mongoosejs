5e1d85b5966ca78cd4979586b5f256270d91d2d5
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-12 14:03:41 +0300

    implement blogs route

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const blogRoutes = require(`${__dirname}/routes/blogRoutes.js`);
 
 const app = express();
 app.use(express.json());
+
+app.use('/blogs', blogRoutes);
 
 module.exports = app;
