f79bfe4e2480ccfd07099b93448c13b83b9279cb
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-12 14:26:39 +0300

    implement schema, model in server.js

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  body: String,
+  comments: [
+    {
+      body: String,
+      date: Date,
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number,
+  },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 port = process.env.PORT;
 app.listen(port, () => {
